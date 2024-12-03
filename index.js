91534497182511a39350725d106ba4ad429cc3ec
Cynthia Mujyambere <cm845@cornell.edu>
2024-12-02 22:41:08 -0500

    minor changes in index.js

--- index.js
+++ index.js
@@ -44,11 +44,11 @@ app.use('/corner/course/question', createQuestionRoutes(io)); // Pass io to rout
 app.use('/corner/user', userRoutes);
 
 // Serve static files
-app.use(express.static(path.join(__dirname, '../corner-app/build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // Serve the index.html file for all routes that are not API routes
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../corner-app/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 // Database Connection
